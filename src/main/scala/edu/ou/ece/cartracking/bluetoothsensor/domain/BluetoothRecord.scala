14d6647fa094ad1906814a5eb4bdbede4983d05c
Bei <libei.twer@gmail.com>
2010-04-16 13:04:42 -0500

    initial commit

---
+++ src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/domain/BluetoothRecord.scala
@@ -0,0 +1,11 @@
+package edu.ou.ece.cartracking.bluetoothsensor
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bei
+ * Date: Apr 16, 2010
+ * Time: 11:51:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class BluetoothRecord
---
+++ src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/repository/BluetoothRecordRepository.scala
@@ -0,0 +1,3 @@
+package edu.ou.ece.cartracking.bluetoothsensor.repository
+
+object BluetoothRecordRepository 
