0cd62fd500b429bfbdc86ea4d7efc58622dfc8ac
Bei <libei.twer@gmail.com>
2010-04-16 13:19:23 -0500

    pack scala/* when deploying.

--- src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/domain/BluetoothRecord.scala
+++ src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/domain/BluetoothRecord.scala
@@ -1,11 +1,3 @@
-package edu.ou.ece.cartracking.bluetoothsensor
-
-/**
- * Created by IntelliJ IDEA.
- * User: bei
- * Date: Apr 16, 2010
- * Time: 11:51:35 AM
- * To change this template use File | Settings | File Templates.
- */
+package edu.ou.ece.cartracking.bluetoothsensor.domain
 
 class BluetoothRecord
