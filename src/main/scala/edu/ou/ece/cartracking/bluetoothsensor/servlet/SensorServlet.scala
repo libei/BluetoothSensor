85a6c2c4c4c31c67c32a6859bc42e7e43c300eb1
Bei <libei.twer@gmail.com>
2010-04-20 18:15:39 -0500

    add jetty support, actually it can run!!

--- src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/servlet/SensorServlet.scala
+++ src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/servlet/SensorServlet.scala
@@ -5,5 +5,6 @@ import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
 class SensorServlet extends HttpServlet {
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
     resp.getWriter.append("Blahblahblah")
+    
   }
 }
