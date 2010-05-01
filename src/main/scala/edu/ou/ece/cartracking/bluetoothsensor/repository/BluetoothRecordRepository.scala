c38693d5c1747ff656a818e68db58e62a25fe3e1
Bei <libei.twer@gmail.com>
2010-05-01 14:20:06 -0500

    generate xml from the db records

--- src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/repository/BluetoothRecordRepository.scala
+++ src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/repository/BluetoothRecordRepository.scala
@@ -4,23 +4,25 @@ import java.sql._
 
 class BluetoothRecordRepository(connection: String) {
 
-  def getRecords(): String = {
+  def getRecords(startTime: Long , endTime:Long): String = {
 
     Class.forName("org.sqlite.JDBC");
     val conn:Connection = DriverManager.getConnection(connection)
     val stat:Statement = conn.createStatement()
-//    val rs: ResultSet = stat.executeQuery("select * from DataIn")
-
-//    while (rs.next()) {
-//      System.out.println("UnitTime" + rs.getInt("UnixTime"));
-//      System.out.println("MacAdd" + rs.getString("MacAdd"));
-//      System.out.println("DevType" + rs.getString("DevType"));
-//      System.out.println("UnitSN" + rs.getString("UnitSN"));
-//      System.out.println("job = " + rs.getString("occupation"));
-//    }
-//    rs.close()
+    val rs: ResultSet = stat.executeQuery("select * from DataIn where UnixTime >" + startTime + " and UnixTime < " + endTime)
+    BluetoothRecordsBuilder.appendTag("records")
+    while (rs.next()) {
+      BluetoothRecordsBuilder.appendTag("record")
+      BluetoothRecordsBuilder.appendElement("UnixTime", rs.getInt("UnixTime").toString)
+      BluetoothRecordsBuilder.appendElement("MacAdd", rs.getString("MacAdd"))
+      BluetoothRecordsBuilder.appendElement("DevType", rs.getString("DevType"))
+      BluetoothRecordsBuilder.appendElement("UnitSN", rs.getString("UnitSN"))
+      BluetoothRecordsBuilder.appendClosingTag("record")
+    }
+    BluetoothRecordsBuilder.appendClosingTag("records")
+    rs.close()
     conn.close()
-    null
+    BluetoothRecordsBuilder.toXML
 
   }
 
--- src/test/scala/edu/ou/ece/cartracking/bluetoothsensor/repository/BluetoothRecordRepositorySpec.scala
+++ src/test/scala/edu/ou/ece/cartracking/bluetoothsensor/repository/BluetoothRecordRepositorySpec.scala
@@ -4,16 +4,26 @@ import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FlatSpec
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
+import java.net.URL
+import java.lang.String
 
 @RunWith(classOf[JUnitRunner])
 class BluetoothRecordRepositorySpec extends FlatSpec with ShouldMatchers {
 
   it should "connect to database" in {
+    val dbPath = this.getClass.getClassLoader.getResource("BlueTooth73.db").getPath
+    val repo = new BluetoothRecordRepository("jdbc:sqlite:" + dbPath)
+    val xml = repo.getRecords(1268223396, 1269263000)
 
-    val repo = new BluetoothRecordRepository("jdbc:sqlite:BlueTooth73.db")
-    repo.getRecords
-    true should equal(true)
-    
+    shouldContain(xml, "<records>")
+    shouldContain(xml, "</records>")
+    shouldContain(xml, "<record><UnixTime>1268223396</UnixTime><MacAdd>00:01:95:09:5A:D3</MacAdd><DevType>DesktopComputer</DevType><UnitSN>001-001-001</UnitSN></record>")
+    shouldContain(xml, "<record><UnixTime>1269262993</UnixTime><MacAdd>00:16:74:4B:0B:29</MacAdd><DevType>HeadsetAudio/Video</DevType><UnitSN>001-001-001</UnitSN></record>")
+
+  }
+
+  def shouldContain(xml: String, substring: String) {
+    xml.contains(substring) should equal(true)
   }
   
 }
