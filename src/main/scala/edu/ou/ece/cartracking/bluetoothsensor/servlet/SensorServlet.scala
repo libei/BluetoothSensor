f4c8a4a90465e43b26cdfe8b5fcc3673736b752d
Bei <libei.twer@gmail.com>
2010-05-01 15:05:18 -0500

    config the db path in the web.xml

--- src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/servlet/SensorServlet.scala
+++ src/main/scala/edu/ou/ece/cartracking/bluetoothsensor/servlet/SensorServlet.scala
@@ -1,10 +1,23 @@
 package edu.ou.ece.cartracking.bluetoothsensor.servlet
 
 import javax.servlet.http.{HttpServletResponse, HttpServletRequest, HttpServlet}
+import edu.ou.ece.cartracking.bluetoothsensor.repository.BluetoothRecordRepository
+import org.apache.log4j.Logger
 
 class SensorServlet extends HttpServlet {
+
+  private val logger: Logger = Logger.getLogger(getClass)
+
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = {
-    resp.getWriter.append("Blahblahblah")
+    resp.setContentType("text/html");
+    val dbPath = getInitParameter("dbpath")
+    logger.debug("database file: " + dbPath)
+    resp.getWriter.append("database file: " + dbPath)
+    val repo = new BluetoothRecordRepository(dbPath)
+    val start = req.getParameter("start").toLong
+    val end = req.getParameter("end").toLong
+    logger.debug("start: " + start + " end: " + end)
+    resp.getWriter.append(repo.getRecords(start, end))
     
   }
 }
